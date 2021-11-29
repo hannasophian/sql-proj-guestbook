589be1398c5c0fcbc49f4937773c9a9da69f6816
Hanna <sophianhanna@gmail.com>
2021-11-29 14:37:19 +0000

    Can enter input and get values

--- client/src/components/SignatureInput.tsx
+++ client/src/components/SignatureInput.tsx
@@ -1,5 +1,38 @@
+import { useState } from "react"
+
 export default function SignatureInput(): JSX.Element {
+    const [message, setMessage] = useState<string>("");
+    const [name, setName] = useState<string>("");
+
     return(
-        <h1>This is the signature input</h1>
+        <div className="signatureinput">
+            <h1>This is the signature input</h1>
+            <label htmlFor="nameInput">Name:</label>
+            <input
+            id = "nameInput"
+            value={name}
+            onChange= {(e) => {
+            setName(e.target.value)
+            }
+            } />
+            <label htmlFor="messageInput">Message:</label>
+
+            <input
+            id="messageInpuut"
+            value={message}
+            onChange= {(e) => {
+            setMessage(e.target.value)
+            }
+            } />
+
+            <button
+                disabled ={name.length === 0}
+                onClick={() => {
+                console.log(name, message);
+                }}
+            >
+                Send
+            </button>
+        </div>
     )
 }
