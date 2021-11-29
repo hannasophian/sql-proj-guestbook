ff9a368addd342da93bdf36a60c073e55f7de64c
Hanna <sophianhanna@gmail.com>
2021-11-29 15:23:04 +0000

    Input works

--- client/src/components/SignatureInput.tsx
+++ client/src/components/SignatureInput.tsx
@@ -1,12 +1,30 @@
-import { useState } from "react"
+import React, { useState } from "react"
+
 
 export default function SignatureInput(): JSX.Element {
     const [message, setMessage] = useState<string>("");
     const [name, setName] = useState<string>("");
 
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>){ 
+        e.preventDefault()
+        try {
+        const body = {name: name, message:message}
+        const response = await fetch("http://localhost:4000/signatures", {
+            method: "POST",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify(body)
+        })
+        console.log(message, name);
+        console.log(response);
+    }   catch(error) {
+        console.log(error)
+    }
+    }
+
     return(
         <div className="signatureinput">
             <h1>This is the signature input</h1>
+            <form className="form" onSubmit={(e) => handleSubmit(e)}>
             <label htmlFor="nameInput">Name:</label>
             <input
             id = "nameInput"
@@ -26,13 +44,10 @@ export default function SignatureInput(): JSX.Element {
             } />
 
             <button
+                type = "submit"
                 disabled ={name.length === 0}
-                onClick={() => {
-                console.log(name, message);
-                }}
-            >
-                Send
-            </button>
+            >Submit</button>
+            </form>
         </div>
     )
 }
